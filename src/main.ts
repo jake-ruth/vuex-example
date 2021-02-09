b64270518dd8e25667cf238b543f0febe4029a42
Jake Ruth <jakeruthmusic@gmail.com>
2021-02-09 18:31:01 -0500

    attempt

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import vueAwesomeCountdown from 'vue-awesome-countdown';
+const vueAwesomeCountdown = require('vue-awesome-countdown');
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
