2504472d9514eef3ae3cf9be6d28210421ede4ee
Jake Ruth <jakeruthmusic@gmail.com>
2021-02-09 12:30:06 -0500

    format time countdown

--- src/components/CountdownTimer.vue
+++ src/components/CountdownTimer.vue
@@ -1,21 +1,24 @@
-<template >
-<countdown :end-time="new Date().getTime() + 60000 * minutes">
-  <template
-    v-slot:process="anyYouWantedScopName">
-      <span>{{ `Lefttime: ${anyYouWantedScopName.timeObj.ceil.s}` }}</span>
+<template>
+  <countdown :end-time="new Date().getTime() + 60000 * minutes">
+    <template v-slot:process="anyYouWantedScopName">
+      <samp>{{ `Lefttime: ${formatTime(anyYouWantedScopName.timeObj.ceil.s)}` }}</samp>
     </template>
-  <template
-    v-slot:finish>
+    <template v-slot:finish>
       <span>Done!</span>
-  </template>
-</countdown>
+    </template>
+  </countdown>
 </template>
 
 <script>
 export default {
-    name: 'CountdownTimer',
-    props: {
-      minutes: Number
+  name: 'CountdownTimer',
+  props: {
+    minutes: Number
+  },
+  methods: {
+    formatTime(seconds) {
+      return `${Math.floor(seconds / 60)} min, ${seconds % 60} sec`;
     }
-}
+  }
+};
 </script>
