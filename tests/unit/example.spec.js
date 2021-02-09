2cbc012ddf6c869209d7531fc916889c09881fa8
Jake Ruth <jakeruthmusic@gmail.com>
2021-02-09 10:20:17 -0500

    test pr should work

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,7 +3,7 @@ import { shallowMount } from '@vue/test-utils';
 describe('Test Suite', () => {
   it('First test', () => {
     let test = 1;
-    let test2 = 2;
+    let test2 = 1;
     expect(test === test2).toBe(true);
   });
 });
