200f0d92a8fa35670b866aaf8f4c2db781005eb3
Jake Ruth <jakeruthmusic@gmail.com>
2021-02-09 08:36:20 -0500

    setting up firebase hosting with ci and cd

---
+++ tests/unit/example.spec.js
@@ -0,0 +1,9 @@
+import { shallowMount } from '@vue/test-utils';
+
+describe('Test Suite', () => {
+  it('First test', () => {
+    let test = 1;
+    let test2 = 1;
+    expect(test === test2);
+  });
+});
